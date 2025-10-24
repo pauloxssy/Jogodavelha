7c8ec05a9d4285144da5cbb0247f297495066218
Atariana <vieiragalvao00130@gmail.com>
2025-10-23 23:53:27 -0300

    adicionando mais opcoes de menu

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
 #include "cores.h"
 
 /* Nome: Sophia Maria Vieira Galvao(Matricula: 202454071) */   
@@ -86,6 +87,32 @@ void Maquina(){
 
 }
 
+void Pikachu(){
+    int i, j;
+
+    do{
+        i = rand() % 3;
+        j = rand() % 3;
+
+    } while(tabuleiro[i][j] != '_');
+        printf(YELLOW"Pikachu atacou na posicao: %d %d\n"RESET, i, j);
+        tabuleiro[i][j] = 'P';
+
+}
+
+void Charmander(){
+    int i, j;
+
+    do{
+        i = rand() % 3;
+        j = rand() % 3;
+
+    } while(tabuleiro[i][j] != '_');
+        printf(RED"Charmander atacou na posicao: %d %d\n"RESET, i, j);
+        tabuleiro[i][j] = 'C';
+
+}
+
 char Resultado(){
     for(int i=0; i<3; i++){
 
@@ -117,14 +144,16 @@ printf(BLUE"\txxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n"RESET);
 printf(MAGENTA"\t\t1- Iniciar Jogo(Jogador 1 x Maquina)\n"RESET);
 printf(MAGENTA"\t\t2 - Modo 2 Jogadores\n"RESET);
 printf(MAGENTA"\t\t3 - Placar\n"RESET);
-printf(MAGENTA"\t\t4 - Sair\n"RESET);
+printf(MAGENTA"\t\t4 - Rinha Pokemon (Treinador(a) X Rival)\n"RESET);
+printf(MAGENTA"\t\t5 - Lucros e Percas das Rinhas (Treinador(a) X Rival)\n"RESET);
+printf(MAGENTA"\t\t6 - Sair\n"RESET);
 printf(BLUE"\txxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n"RESET);
 
 }
 
 int main(){
     int opcao, moeda = Cara_Coroa(),placar_j1 = 0, placar_cpu = 0, placar_j2 = 0, placar_empt_j1_cpu = 0, placar_empt_j1_j2 = 0; 
-    char sair = 'N', ganhador = ' ';
+    char sair = 'N', ganhador = ' ', lucro_mon_ganho_t = 0, lucro_mon_perdido_t = 0, lucro_mon_ganho_r = 0, lucro_mon_perdido_r = 0;
     srand(time(0));
 
     while(sair == 'N' || sair == 'n'){
@@ -258,6 +287,105 @@ int main(){
         }//opcao 3
 
         else if(opcao == 4){
+            printf(BLUE"Ola treinador(a)! O seu pokemon para a rinha ser o Pikachu e o do seu rival sera o Charmander.\n"RESET);
+            printf(BLUE"Quem ganha recebe R$10 reais. Quem perder perde 10. E caso der empate, ambos ganham R$5 reais\n"RESET);
+
+            CriandoTabuleiro();
+            sleep(1);
+            ImprimindoTabuleiro();
+            sleep(1);
+            printf(BLUE"Lancando a moeda pokemon para sortear(Treinador(a) = cara e Rival = coroa)...\n"RESET);
+
+            if (moeda == 0){
+                printf(YELLOW"Cara! O Treeinador(a) comeca!\n"RESET);
+                sleep(1);
+                for (int t = 0; t < 9 && ganhador == ' '; t++){
+
+                    if(t % 2 == 0){
+                        Pikachu();
+                        sleep(1); 
+                    } 
+                    else{ 
+                        Charmander();
+                        sleep(1);
+                    }
+
+                    ImprimindoTabuleiro();
+                    sleep(1);
+                    ganhador = Resultado();
+                    sleep(1);
+
+                }
+            }
+
+            else{
+                    printf(RED"Coroa! O Rival comeca!\n"RESET);
+                    sleep(1);
+                    for (int t = 1; t < 10 && ganhador == ' '; t++){
+
+                        if(t % 2 != 0){
+                            Charmander();
+                            sleep(1);
+                        }
+                        else{
+                            Pikachu();
+                            sleep(1);
+                        }
+
+                        ImprimindoTabuleiro();
+                        sleep(1);
+                        ganhador = Resultado();
+                        sleep(1);
+        
+                    }
+
+            }
+
+            //Verificando resultado Treinador X Rival
+            if(ganhador == 'P'){
+                    sleep(1);
+                    printf(YELLOW"Pikachu ganhou a rinha! Voce ganhou R$10 reais.\n"RESET);
+                    sleep(1);
+                    lucro_mon_ganho_t = lucro_mon_ganho_t + 10;
+                    sleep(1);
+                    lucro_mon_perdido_r = lucro_mon_perdido_r + 10;
+                    sleep(1);
+            }
+            if(ganhador == 'C'){
+                    sleep(1);
+                    printf(RED"Rival venceu! Voce perdeu R$10 reais\n"RESET);
+                    sleep(1);
+                    lucro_mon_perdido_t = lucro_mon_perdido_t + 10;
+                    sleep(1);
+                    lucro_mon_ganho_r = lucro_mon_ganho_r + 10;
+                    sleep(1);
+            }
+            if(ganhador != 'P' && ganhador != 'C' && ganhador != ' '){
+                sleep(1);
+                printf(WHITE"Empate! Ambos vao ganhar R$5 reais.\n"RESET);
+                sleep(1);
+                lucro_mon_ganho_t = lucro_mon_ganho_t + 5;
+                sleep(1);
+                lucro_mon_ganho_r = lucro_mon_ganho_r + 5;
+                sleep(1);
+
+            }
+            
+            
+
+        }//opcao 4
+
+        else if(opcao == 5){
+            printf(WHITE"Lucros e Percas das Rinhas:\n"RESET);
+            printf(YELLOW"Treinador(a) ganhou: R$%d reais\n"RESET, lucro_mon_ganho_t);
+            printf(YELLOW"Treinador(a) perdeu: R$%d reias\n"RESET, lucro_mon_perdido_t);
+            printf(RED"Rival ganhou: R$%d\n"RESET, lucro_mon_ganho_r);
+            printf(RED"Rival perdeu: %d\n"RESET, lucro_mon_perdido_r);
+
+        }//opcao 5
+
+
+        else if(opcao == 6){
             printf(YELLOW"Voce quer realmente sair?:"RESET);
             scanf(" %c", &sair);
 
@@ -266,7 +394,7 @@ int main(){
                 break;
             }
 
-        }//opcao 4
+        }//opcao 6
 
         else{
             printf(RED"Nao tem essa opcao. Por favor digite novamente.\n\n"RESET);
@@ -276,21 +404,6 @@ int main(){
 
     }//fim do while
 
-    
-
-    
-
-    //opcoes de menu:
-    //1- Iniciar Jogo(Jogador 1 x Maquina);
-    //2 - Modo 2 Jogadores;
-    //3 - Placar (Jogador 1, Jogador 2 e Máquina);
-    //4 - Sair(Iremos usar o comando exit());
-
- //Antes de Iniciar o Jogo(Seja na opcao 1 ou na opcao 2) devemos sortear quem vai começar;
-
-    
-        
-    
 
     system("pause");
     return 0;
